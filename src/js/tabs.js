eb758bf97cf3b8abdc8be9e074b1df55a3ce1283
Vitalii <volfram@ukr.net>
2022-07-26 03:23:23 +0300

    tabs are working

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,12 +1,41 @@
-const tab = document.querySelectorAll('.catalog__tab');
-const content = document.querySelectorAll('.catalog__content');
+// const tab = document.querySelectorAll('.catalog__tab');
 
-function activeTab()
-{
-    tab.forEach((item) =>
-    item.classList.remove('catalog__tab_active'));
-    this.classList.add('catalog__tab_active');
-}
+// function activeTab()
+// {
+//     tab.forEach((item) => {
+//       item.classList.remove('catalog__tab_active');
+//     });
+//     this.classList.add('catalog__tab_active');
+// }
 
-tab.forEach((item) =>
-item.addEventListener('click', activeTab));
+// tab.forEach((item) =>
+// item.addEventListener('click', activeTab));
+
+let tab = function () {
+  let tabNav = document.querySelectorAll('.catalog__tab'),
+      tabContent = document.querySelectorAll('.catalog__content'),
+      tabName;
+
+  tabNav.forEach(item => {
+      item.addEventListener('click', selectTabNav)
+  });
+
+  function selectTabNav() {
+      tabNav.forEach(item => {
+          item.classList.remove('catalog__tab_active');
+      });
+      this.classList.add('catalog__tab_active');
+      tabName = this.getAttribute('data-tab-name');
+      selectTabContent(tabName);
+  }
+
+  function selectTabContent(tabName) {
+      tabContent.forEach(item => {
+          item.classList.contains(tabName) ? item.classList.add('catalog__content_active') : item.classList.remove('catalog__content_active');
+      })
+  }
+
+};
+
+
+tab();
